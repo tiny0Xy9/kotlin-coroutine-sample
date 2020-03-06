bc7b817126cfd7766db6117c9d2c49d5ee3302f0
tiny <tinyLab0Xy9@gmail.com>
2020-03-06 23:27:13 +0900

    button de kidou

--- CoroutineSample/app/src/main/java/app/pete/com/coroutinesample/MainActivity.kt
+++ CoroutineSample/app/src/main/java/app/pete/com/coroutinesample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val model =  ViewModelProvider.NewInstanceFactory().create(MainActivityViewModel::class.java)
         lifecycle.addObserver(model)
+        //todo syntheticはライフサイクルで作り直された時になどに、nullになる可能性があるため、非推奨
+        startcoroutine.setOnClickListener {
+            startActivity(Intent(this,CoroutineSampleActivity::class.java))
+        }
 
-        startActivity(Intent(this,CoroutineSampleActivity::class.java))
 
     }
 }
